96adba18feabaaf9130ec8ad1c5b0451fb642e6d
ArtevelizDatabase <artevelizproject@gmail.com>
2025-10-07 15:18:15 +0700

    Pesan commit kamu

---
+++ assets/js/portfolio-data.js
@@ -0,0 +1,135 @@
+const portfolioData = [
+  {
+    id: 1,
+    slug: "kashtech-doyel",
+    title: "asd",
+    year: "23",
+    category: "Design",
+    author: "Asem",
+    description: "In 2025, MACreative partnered with Kashtech Doyel to create a modern and innovative brand identity. We delivered a comprehensive branding solution, including logo design, visual identity, and digital assets tailored for a global audience.",
+    images: [
+    "assets/imgs/portfolio/id1/Portfolio1.webp",
+    "assets/imgs/portfolio/id1/Portfolio2.webp",
+    "assets/imgs/portfolio/id1/Portfolio3.webp",
+    "assets/imgs/portfolio/id1/Portfolio4.webp",
+    "assets/imgs/portfolio/id1/Portfolio5.webp",
+    "assets/imgs/portfolio/id1/Portfolio6.webp"
+    ],
+    client: "Kashtech Company",
+    services: ["Brand Identity", "Logo Design", "Visual Identity", "Digital Assets"]
+  },
+  {
+    id: 2,
+    slug: "saudi-lime-green",
+    title: "Saudi Lime Green",
+    year: "2025",
+    category: "Design",
+    author: "Jerry",
+    description: "This project involved creating a vibrant and eco-friendly brand identity for Saudi Lime Green, focusing on sustainable packaging and digital marketing solutions.",
+    images: [
+    "assets/imgs/portfolio/id2/Portfolio1.webp",
+    "assets/imgs/portfolio/id2/Portfolio2.webp",
+    "assets/imgs/portfolio/id2/Portfolio3.webp",
+    "assets/imgs/portfolio/id2/Portfolio4.webp",
+    "assets/imgs/portfolio/id2/Portfolio5.webp",
+    "assets/imgs/portfolio/id2/Portfolio6.webp"
+    ],
+    client: "Saudi Lime Company",
+    services: ["Packaging Design", "Digital Marketing"]
+  },
+  {
+    id: 3,
+    slug: "saudi-venture-capital",
+    title: "Business Multipurpose Presentation",
+    year: "2023",
+    category: "Business",
+    author: "Jerry",
+    description: "In 2023, MACreative partnered with Saudi Venture Capital to develop a bold and impactful brand identity. Our goal was to create a visual language that reflects the innovative spirit of venture capital while maintaining a timeless and professional aesthetic. We delivered a comprehensive brand system, including logo design, typography, and digital assets, tailored to resonate with a global audience.",
+    images: [
+    "assets/imgs/portfolio/id3/Portfolio1.webp",
+    "assets/imgs/portfolio/id3/Portfolio2.gif",
+    "assets/imgs/portfolio/id3/Portfolio3.webp",
+    "assets/imgs/portfolio/id3/Portfolio4.webp",
+    "assets/imgs/portfolio/id3/Portfolio5.webp",
+    "assets/imgs/portfolio/id3/Portfolio6.webp"
+    ],
+    client: "Saudi Venture",
+    services: ["Brand Development", "UX/UI Design", "Front-end Development", "Copywriting", "Shopify Development"]
+  },
+  {
+    id: 4,
+    slug: "Loud-Brew-Coffee-Branding",
+    title: "LOUD BREW | Coffee Branding",
+    year: "2025",
+    category: "Branding",
+    author: "Jerry",
+    description: "In 2023, MACreative partnered with Saudi Venture Capital to develop a bold and impactful brand identity. Our goal was to create a visual language that reflects the innovative spirit of venture capital while maintaining a timeless and professional aesthetic. We delivered a comprehensive brand system, including logo design, typography, and digital assets, tailored to resonate with a global audience.",
+    images: [
+    "assets/imgs/portfolio/id4/Portfolio1.gif",
+    "assets/imgs/portfolio/id4/Portfolio2.webp",
+    "assets/imgs/portfolio/id4/Portfolio3.webp",
+    "assets/imgs/portfolio/id4/Portfolio4.webp",
+    "assets/imgs/portfolio/id4/Portfolio5.webp",
+    "assets/imgs/portfolio/id4/Portfolio6.webp"
+    ],
+    client: "Saudi Venture",
+    services: ["Brand Development", "UX/UI Design", "Front-end Development", "Copywriting", "Shopify Development"]
+  },
+  {
+  id: 5,
+  slug: "saudi-venture-capital",
+  title: "Saudi Venture Capital",
+  year: "2023",
+  category: "Business",
+  author: "Jerry",
+  description: "In 2023, MACreative partnered with Saudi Venture Capital to develop a bold and impactful brand identity. Our goal was to create a visual language that reflects the innovative spirit of venture capital while maintaining a timeless and professional aesthetic. We delivered a comprehensive brand system, including logo design, typography, and digital assets, tailored to resonate with a global audience.",
+  images: [
+    "assets/imgs/portfolio/id5/Portfolio1.webp",
+    "assets/imgs/portfolio/id5/Portfolio2.webp",
+    "assets/imgs/portfolio/id5/Portfolio3.webp",
+    "assets/imgs/portfolio/id5/Portfolio4.webp",
+    "assets/imgs/portfolio/id5/Portfolio5.webp",
+    "assets/imgs/portfolio/id5/Portfolio6.webp"
+  ],
+  client: "Saudi Venture",
+  services: ["Brand Development", "UX/UI Design", "Front-end Development", "Copywriting", "Shopify Development"]
+  },
+  {
+  id: 6,
+  slug: "Sticky-Milk-Branding-Packaging",
+  title: "Sticky Milk - Branding & Packaging",
+  year: "2025",
+  category: "Branding",
+  author: "Jerry",
+  description: "In 2023, MACreative partnered with Saudi Venture Capital to develop a bold and impactful brand identity. Our goal was to create a visual language that reflects the innovative spirit of venture capital while maintaining a timeless and professional aesthetic. We delivered a comprehensive brand system, including logo design, typography, and digital assets, tailored to resonate with a global audience.",
+  images: [
+    "assets/imgs/portfolio/id6/Portfolio1.webp",
+    "assets/imgs/portfolio/id6/Portfolio2.webp",
+    "assets/imgs/portfolio/id6/Portfolio3.webp",
+    "assets/imgs/portfolio/id6/Portfolio4.webp",
+    "assets/imgs/portfolio/id6/Portfolio5.webp",
+    "assets/imgs/portfolio/id6/Portfolio6.webp"
+  ],
+  client: "Saudi Venture",
+  services: ["Brand Development", "UX/UI Design", "Front-end Development", "Copywriting", "Shopify Development"]
+  },
+  {
+  id: 7,
+  slug: "saudi-venture-capital",
+  title: "Saudi Venture Capital",
+  year: "2023",
+  category: "Business",
+  author: "Jerry",
+  description: "In 2023, MACreative partnered with Saudi Venture Capital to develop a bold and impactful brand identity. Our goal was to create a visual language that reflects the innovative spirit of venture capital while maintaining a timeless and professional aesthetic. We delivered a comprehensive brand system, including logo design, typography, and digital assets, tailored to resonate with a global audience.",
+  images: [
+    "assets/imgs/portfolio/id7/Portfolio1.webp",
+    "assets/imgs/portfolio/id7/Portfolio2.webp",
+    "assets/imgs/portfolio/id7/Portfolio3.webp",
+    "assets/imgs/portfolio/id7/Portfolio4.webp",
+    "assets/imgs/portfolio/id7/Portfolio5.webp",
+    "assets/imgs/portfolio/id7/Portfolio6.webp"
+  ],
+  client: "Saudi Venture",
+  services: ["Brand Development", "UX/UI Design", "Front-end Development", "Copywriting", "Shopify Development"]
+  }
+];
